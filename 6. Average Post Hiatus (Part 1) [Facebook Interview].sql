0881532570c3700c45e623560f106d2f728f659c
benemana <56397465+benemana@users.noreply.github.com>
2023-03-23 12:01:05 +0100

    Update 6. Average Post Hiatus (Part 1) [Facebook Interview].sql

--- 6. Average Post Hiatus (Part 1) [Facebook Interview].sql
+++ 6. Average Post Hiatus (Part 1) [Facebook Interview].sql
@@ -1,4 +1,6 @@
 /*
+Level: EASY
+
 Given a table of Facebook posts, for each user who posted at least twice in 2021, 
 write a query to find the number of days between each user’s first post of the year and last post of the year in the year 2021. 
 Output the user and number of the days between each user's first and last post.
