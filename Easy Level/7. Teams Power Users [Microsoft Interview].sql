003f49096e536562bf4a1959a738fa2e99e075da
benemana <56397465+benemana@users.noreply.github.com>
2023-03-23 12:22:05 +0100

    Update 7. Teams Power Users [Microsoft Interview].sql

--- Easy Level/7. Teams Power Users [Microsoft Interview].sql
+++ Easy Level/7. Teams Power Users [Microsoft Interview].sql
@@ -1,4 +1,5 @@
 /*
+Level: EASY
 Write a query to find the top 2 power users who sent the most messages on Microsoft Teams in August 2022. 
 Display the IDs of these 2 users along with the total number of messages they sent. 
 Output the results in descending count of the messages.
