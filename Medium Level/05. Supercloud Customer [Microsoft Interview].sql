23f8721af57ee0b383fdf0d47d9bd43e683d9878
benemana <56397465+benemana@users.noreply.github.com>
2023-03-28 10:47:17 +0200

    Update 05. Supercloud Customer [Microsoft Interview].sql

--- Medium Level/05. Supercloud Customer [Microsoft Interview].sql
+++ Medium Level/05. Supercloud Customer [Microsoft Interview].sql
@@ -12,11 +12,11 @@ amount		  integer
 
 customer_contracts Example Input:
 customer_id	product_id	amount
-1			      1			      1000
-1			3			2000
-1			5			1500
-2			2			3000
-2			6			2000
+1		1			1000
+1		3			2000
+1		5			1500
+2		2			3000
+2		6			2000
 
 products Table:
 Column_Name			Type
